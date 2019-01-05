0fb38a92e14efd22ed7ec527755305ff406fed30
Shashank Srivastava <shashanksri99@yahoo.com>
2019-01-05 18:37:07 +0530

    Make_Models
    Making models in Django

--- django-practice/ProTwo/appTwo/admin.py
+++ django-practice/ProTwo/appTwo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from appTwo.models import Webpage,AccessRecord,Topic
 # Register your models here.
+admin.site.register(Webpage)
+admin.site.register(AccessRecord)
+admin.site.register(Topic)
